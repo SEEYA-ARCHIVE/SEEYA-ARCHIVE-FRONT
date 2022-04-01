cedd6809180e6191399eda28e4fdec733d98e629
hayoung123 <proshy1@naver.com>
2022-04-02 01:47:47 +0900

    feat: 회색 emptybox 제거

--- src/components/mocules/imgViewer/ImgViewer.tsx
+++ src/components/mocules/imgViewer/ImgViewer.tsx
@@ -15,7 +15,6 @@ export const ImgViewer: FC<Props> = ({ imgList, width = 500, height = 400 }) => 
   const [selectedIdx, setSelectedIdx] = useState(0);
 
   const minWidth = Math.floor((width - GAP * (MAX_IMG_COUNT - 1)) / 5);
-  const emptyCount = MAX_IMG_COUNT - imgList.length < 0 ? 0 : MAX_IMG_COUNT - imgList.length;
 
   const handleImgClick = (idx: number) => setSelectedIdx(idx);
 
@@ -35,8 +34,6 @@ export const ImgViewer: FC<Props> = ({ imgList, width = 500, height = 400 }) => 
             />
           );
         })}
-        {emptyCount !== 0 &&
-          new Array(emptyCount).fill(0).map((_, idx) => <S.EmptyImg key={idx} width={minWidth} height={minWidth} />)}
       </S.MinImgWrapper>
     </S.Wrapper>
   );
