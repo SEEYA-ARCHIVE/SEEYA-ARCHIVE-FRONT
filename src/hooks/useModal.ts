ba5d515c4a9849c1c3afc717476501e27cb26a26
zih0 <ziho@kakao.com>
2022-04-02 21:16:11 +0900

    feat: 모달 구현 방식 수정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,21 +1,34 @@
-import { useSetRecoilState } from 'recoil';
-import { IModal } from 'src/components/Modal/ModalManager';
+import { Component, ReactElement } from 'react';
+import { modalBlurState } from './../stores/modal';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { modalListState } from 'src/stores/modal';
+import { IOptions, ModalType } from 'src/components/Modal/Modal';
 
 interface IUseModal {
-  openModal: ({ key, props }: IModal) => void;
+  openModal: (component: ModalType, options?: IOptions) => void;
   closeCurrentModal: () => void;
 }
 
 const useModal = (): IUseModal => {
   const setModalList = useSetRecoilState(modalListState);
+  const [blur, setBlur] = useRecoilState(modalBlurState);
 
-  const openModal = ({ key, props }: IModal) => {
-    setModalList((prev) => [...prev, { key, props }]);
+  const openModal = (component: ModalType, options?: IOptions) => {
+    setModalList((prev) => [...prev, component]);
+
+    if (options) manageOptions(options);
   };
 
   const closeCurrentModal = () => {
+    if (blur) setBlur(false);
+
     setModalList((prev) => [...prev].slice(0, prev.length - 1));
+  };
+
+  const manageOptions = (options: IOptions) => {
+    const { blur } = options;
+
+    if (blur) setBlur(true);
   };
 
   return {
--- src/stores/modal.ts
+++ src/stores/modal.ts
@@ -1,7 +1,12 @@
 import { atom } from 'recoil';
-import { IModal } from 'src/components/Modal/ModalManager';
+import { ModalType } from 'src/components/Modal/Modal';
 
-export const modalListState = atom<IModal[]>({
+export const modalListState = atom<ModalType[]>({
   key: 'modalListState',
   default: [],
+});
+
+export const modalBlurState = atom({
+  key: 'modalBlurState',
+  default: false,
 });
