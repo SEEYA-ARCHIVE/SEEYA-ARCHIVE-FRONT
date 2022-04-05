772cef9ce4655ee6a3cfb8a654ef6971e3b36397
kyle <67357426+hayoung123@users.noreply.github.com>
2022-04-05 22:22:28 +0900

    [Feature/#4] FAB버튼 구현 (#14)
    * feat: FAB 버튼틀 구현
    
    * feat: fab버튼 common 폴더로 변경
    
    * feat: FAB버튼 스타일링
    
    * feat: Fab button 위치 css 수정

---
+++ src/components/common/FABButton/FABButton.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import { ColorType } from 'src/types/commonType';
+import styled, { css } from 'styled-components';
+import Icon from '../icon/Icon';
+
+interface Props {
+  bgColor: ColorType;
+  position?: {
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+  };
+  value: string;
+}
+
+export const FABButton: FC<Props> = ({ bgColor, position, value }) => {
+  return (
+    <FABWrapper bgColor={bgColor} position={position}>
+      <div>
+        <Icon name="iconComment" />
+      </div>
+      {value}
+    </FABWrapper>
+  );
+};
+
+const FABWrapper = styled.button<Pick<Props, 'bgColor' | 'position'>>`
+  position: fixed;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  border: none;
+  background-color: ${({ theme, bgColor }) => theme.color[bgColor]};
+
+  font-size: 12px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.color.white};
+
+  ${({ position }) =>
+    css`
+      ${position?.top && `top: ${position.top}px;`}
+      ${position?.right && `right: ${position.right}px;`}
+      ${position?.bottom && `bottom: ${position.bottom}px;`}
+      ${position?.left && `left: ${position.left}px;`}
+    `}
+`;
