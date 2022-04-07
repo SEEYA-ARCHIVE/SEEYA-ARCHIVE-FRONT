021870a2e6597ad245e07d713aea153939e9f9c0
zih0 <ziho@kakao.com>
2022-04-08 00:45:48 +0900

    feat: 리뷰리스트 모달 UI 구현

--- src/components/common/icon/iconPath.ts
+++ src/components/common/icon/iconPath.ts
@@ -1,3 +1,4 @@
 export { default as iconComment } from 'public/assets/icon/Comments.svg';
 export { default as iconCaret } from 'public/assets/icon/Caret.svg';
 export { default as iconThumbsUp } from 'public/assets/icon/ThumbsUp.svg';
+export { default as iconCircleChevronRight } from 'public/assets/icon/CircleChevronRight.svg';
