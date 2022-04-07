9cbadf9c4772cceb3f5315272bb89743b38e8888
zih0 <ziho@kakao.com>
2022-04-08 00:13:55 +0900

    feat: common Tag 컴포넌트 구현

---
+++ src/components/common/tag/Tag.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  text: string;
+}
+
+export const Tag: FC<Props> = ({ text }) => {
+  return <Wrapper>{text}</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  background-color: #f5f5f5;
+  color: #7b7b7b;
+  border-radius: 9px;
+  padding: 3px 8px;
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 12px;
+`;
