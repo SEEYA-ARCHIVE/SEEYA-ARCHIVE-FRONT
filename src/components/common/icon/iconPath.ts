266c534325e585bf1c3f7f69a512d2358055dfd7
zih0 <ziho@kakao.com>
2022-04-16 14:51:44 +0900

    feat: 헤더 타이포 로고 추가

--- src/components/common/icon/iconPath.ts
+++ src/components/common/icon/iconPath.ts
@@ -3,3 +3,4 @@ export { default as iconCaret } from 'public/assets/icon/Caret.svg';
 export { default as iconThumbsUp } from 'public/assets/icon/ThumbsUp.svg';
 export { default as iconCircleChevronRight } from 'public/assets/icon/CircleChevronRight.svg';
 export { default as iconPlus } from 'public/assets/icon/Plus.svg';
+export { default as iconTypoLogo } from 'public/assets/icon/TypoLogo.svg';
