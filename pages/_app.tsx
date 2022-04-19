50b2b154546c91067a0c5c56eb2faa83ad8abb99
송하영 <proshy@woowahan.com>
2022-04-19 21:24:42 +0900

    feat: 임시 레이아웃

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle } from 'src/styles/global-style';
 import { theme } from 'src/styles/theme';
@@ -8,12 +8,28 @@ import { Modal } from 'src/components/common/modal/Modal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <Modal />
-      </ThemeProvider>
-    </RecoilRoot>
+    <>
+      <RecoilRoot>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Wrap>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </Wrap>
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
   );
 }
+
+const Wrap = styled.div`
+  background-color: rgb(0, 14, 86);
+  height: 100vh;
+`;
+const Layout = styled.div`
+  max-width: 1280px;
+  height: 100vh;
+  margin: auto;
+  background-color: ${({ theme }) => theme.color.white};
+`;
