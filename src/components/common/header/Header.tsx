aa1e4ffee20f4b410fa8ac9e626b6ba5cd27b169
Jiho Shin <ziho@kakao.com>
2022-04-19 20:04:51 +0900

    [Feature#24] 헤더 타이포 로고 적용 (#25)
    * feat: 헤더 타이포 로고 추가
    
    * style: 헤더 max-width 지정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,40 +1,38 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import Icon from '../icon/Icon';
 
 interface Props {}
 
 export const Header: FC<Props> = () => {
   return (
     <HeaderWrapper>
-      <MainTitle>시야 아카이브</MainTitle>
-      <SubTitle>SEEYA-ARCHIVE</SubTitle>
+      <Nav>
+        <Icon name="iconTypoLogo" />
+      </Nav>
     </HeaderWrapper>
   );
 };
 
 const HeaderWrapper = styled.div`
   width: 100vw;
-  height: 60px;
-  background-color: ${({ theme }) => theme.color.gray5};
+  height: 80px;
 
   display: flex;
-  align-items: center;
-
-  padding: 15px 50px;
+  justify-content: center;
 
   position: absolute;
   top: 0;
   left: 0;
 `;
 
-const MainTitle = styled.span`
-  font-family: 'Black Han Sans', sans-serif;
-  font-size: 30px;
-  margin-right: 16px;
-`;
+const Nav = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1280px;
 
-const SubTitle = styled.span`
-  font-family: 'Poppins', sans-serif;
-  color: ${({ theme }) => theme.fontColor.gray};
-  letter-spacing: 0.2em;
+  display: flex;
+  align-items: center;
+
+  padding: 20px;
 `;
