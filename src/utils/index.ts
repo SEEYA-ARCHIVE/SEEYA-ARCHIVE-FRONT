c82023c32bfd770d3be2b5dbc55be3c0cd8f97f4
송하영 <proshy@woowahan.com>
2022-04-19 20:22:23 +0900

    feat: 비교박스 컴포넌트 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,8 @@
-export {};
+export const debounce = (fn: Function, wait: number) => {
+  let timer: ReturnType<typeof setTimeout>;
+  function debounced(e: any) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => fn(e), wait);
+  }
+  return debounced;
+};
