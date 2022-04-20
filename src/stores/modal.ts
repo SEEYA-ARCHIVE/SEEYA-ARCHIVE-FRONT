f1ac3cd69ec9282d6d93a799fafc2c2d4d6c3507
zih0 <ziho@kakao.com>
2022-04-20 23:10:46 +0900

    feat: 모달 고차컴포넌트에서 모달 배경 옵션 관리

--- src/stores/modal.ts
+++ src/stores/modal.ts
@@ -5,18 +5,3 @@ export const modalListState = atom<ReactElement[]>({
   key: 'modalList',
   default: [],
 });
-
-export const modalBackgroundTransparentState = atom({
-  key: 'modalBackgroundTransparent',
-  default: false,
-});
-
-export const modalBackgroundBlurState = atom({
-  key: 'modalBackgroundBlur',
-  default: false,
-});
-
-export const modalBackgroundLockState = atom({
-  key: 'modalBackgroundLock',
-  default: false,
-});
