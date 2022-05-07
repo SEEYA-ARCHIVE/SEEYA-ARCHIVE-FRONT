b688cb1771a8bbacd94b1606abef61dec2d66aaf
zih0 <ziho@kakao.com>
2022-05-07 19:57:51 +0900

    chore: 리뷰 타입 파일 이동

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,6 +1,6 @@
-import { IReviewListResponse } from 'src/types/api';
+import { IReviewListType } from 'src/types/api/review';
 import axios from 'axios';
-export const getReviewListAPI = async (seatId: number, page: number): Promise<IReviewListResponse> => {
+export const getReviewListAPI = async (seatId: number, page: number): Promise<IReviewListType> => {
   const { data } = await axios.get(`http://3.36.62.207/seat_areas/${seatId}/reviews?page=${page}`);
 
   return data;
