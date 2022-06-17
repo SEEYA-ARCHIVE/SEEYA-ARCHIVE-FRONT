3608e27572b35ffab09c6a6215adfb5b25b24393
Jiho Shin <ziho@planningo.io>
2022-06-17 21:34:54 +0900

    feat: 로그인 관련 이미지 추가

--- src/components/common/image/imagePath.ts
+++ src/components/common/image/imagePath.ts
@@ -1,2 +1,4 @@
 export { default as sample } from 'public/assets/image/sample.jpeg';
 export { default as sampleThumbnail } from 'public/assets/image/sample-thumbnail.jpeg';
+export { default as AuthImage } from 'public/assets/image/AuthImage.png';
+export { default as HeartBubble } from 'public/assets/image/HeartBubble.png';
