8e7b01374bdb5715cda6567b4ba9dbaebbfec99e
zih0 <ziho@kakao.com>
2022-07-22 01:28:29 +0900

    feat: 로그아웃 연결

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import React, { FC } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 
 import { ROUTE } from 'src/route';
 import { userSessionState } from 'src/stores/user';
@@ -13,9 +13,7 @@ export const Header: FC<Props> = () => {
   const router = useRouter();
   const [userSession, setUserSession] = useRecoilState(userSessionState);
 
-  const logout = () => {
-    setUserSession(undefined);
-  };
+  const logout = () => {};
 
   return (
     <HeaderWrapper>
@@ -28,8 +26,10 @@ export const Header: FC<Props> = () => {
           }}
         />
         <AuthMenu>
-          {userSession ? (
-            <HeaderButton onClick={logout}>LOG OUT</HeaderButton>
+          {!userSession ? (
+            <HeaderButton onClick={logout}>
+              <a href="https://api.seeya-archive.com/kakao_logout">LOG OUT</a>
+            </HeaderButton>
           ) : (
             <HeaderButton
               onClick={() => {
@@ -90,4 +90,8 @@ const HeaderButton = styled.button`
   background-color: ${({ theme }) => theme.color.white};
   color: #376f77;
   border: 1px solid #688f95;
+
+  a {
+    color: #376f77;
+  }
 `;
