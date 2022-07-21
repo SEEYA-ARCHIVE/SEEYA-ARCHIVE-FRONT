29ecae4336578ff3a0659e5e40ff5083b3a79732
Jiho Shin <ziho@kakao.com>
2022-07-22 01:48:55 +0900

    헤더 세션 유무 분기처리 반대로 되어있던거 다시 수정 (#73)
    * feat: 헤더 임시 로그아웃 구현
    
    * feat: 로그아웃 연결
    
    * fix : 헤더 세션 유무 분기처리 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -13,8 +13,6 @@ export const Header: FC<Props> = () => {
   const router = useRouter();
   const [userSession, setUserSession] = useRecoilState(userSessionState);
 
-  const logout = () => {};
-
   return (
     <HeaderWrapper>
       <Nav>
@@ -26,8 +24,8 @@ export const Header: FC<Props> = () => {
           }}
         />
         <AuthMenu>
-          {!userSession ? (
-            <HeaderButton onClick={logout}>
+          {userSession ? (
+            <HeaderButton>
               <a href="https://api.seeya-archive.com/kakao_logout">LOG OUT</a>
             </HeaderButton>
           ) : (
