92834e76cbf8499f99d1ba2c7ebebb8abc301d91
kyle <67357426+hayoung123@users.noreply.github.com>
2022-07-23 12:44:51 +0900

    feat: 업로드 페이지 디테일 UI 수정 (#77)

--- src/components/upload/UploadFormSeats.tsx
+++ src/components/upload/UploadFormSeats.tsx
@@ -14,7 +14,12 @@ interface Props {
 export const UploadFormSeats: FC<Props> = ({ onChangeSeatAreaId }) => {
   const [hallList, setHallList] = useState<HallListType>([]);
   const [selectedHall, setSelectedHall] = useState<number>();
+  const [selectedSeatArea, setSelectedSeatArea] = useState<{ floor: number; area: string }>();
   const [seatsData, setSeatsData] = useState<SeatAreaType[]>([]);
+
+  const setSelectedArea = (floor: number, area: string) => {
+    setSelectedSeatArea({ floor, area });
+  };
 
   const getHallList = async () => {
     const data = await getHallListAPI();
@@ -38,18 +43,26 @@ export const UploadFormSeats: FC<Props> = ({ onChangeSeatAreaId }) => {
   return (
     <Wrap>
       <Title>공연장</Title>
-      <Select
-        value={selectedHall + ''}
-        onChange={(selectHallId) => {
-          setSelectedHall(+selectHallId);
-        }}
-        options={hallList.map((hall) => ({ label: hall.name, value: hall.concertHallId + '' }))}
-      />
+      <SeatDetailWrap>
+        <Select
+          value={selectedHall + ''}
+          onChange={(selectHallId) => {
+            setSelectedHall(+selectHallId);
+          }}
+          options={hallList.map((hall) => ({ label: hall.name, value: hall.concertHallId + '' }))}
+        />
+        {selectedSeatArea && (
+          <SeatDetail>
+            {selectedSeatArea.floor}층 {selectedSeatArea.area.toUpperCase()}
+          </SeatDetail>
+        )}
+      </SeatDetailWrap>
       <UploadSeats
         hallId={selectedHall ?? 1}
         data={uploadOlympicData}
         seatsData={seatsData}
         onChangeSeatAreaId={onChangeSeatAreaId}
+        setSelectedArea={setSelectedArea}
       />
     </Wrap>
   );
@@ -63,4 +76,15 @@ const Wrap = styled.div`
 const Title = styled.div`
   margin-bottom: 12px;
   font-weight: 700;
+`;
+
+const SeatDetailWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SeatDetail = styled.div`
+  margin-left: 8px;
+  font-weight: 700;
+  font-size: 14px;
 `;
