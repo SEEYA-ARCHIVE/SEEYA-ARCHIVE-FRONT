52d3e0b83fce1d2ed3e12fedc8bef01c9693bcf7
zih0 <ziho@kakao.com>
2022-07-23 12:25:24 +0900

    feat: axios csrf 토큰 설정 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,4 +4,6 @@ export const initAxiosConfig = () => {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_HOST;
   axios.defaults.timeout = 3000;
   axios.defaults.withCredentials = true;
+  axios.defaults.xsrfCookieName = 'csrftoken';
+  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 };
